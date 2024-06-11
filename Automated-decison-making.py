a4d855024186e16d35810245ddf6173715d68fe9
Jmuthoga <johnmuthogakanyingi@gmail.com>
2024-06-11 12:36:25 +0300

    first commit

---
+++ Automated-decison-making.py
@@ -0,0 +1,10 @@
+# Hello all. In this program tutorial I will show you how to code a very simple random decision maker
+# that is greate for decision what type of ffod to prepare for dinner
+
+from random import choice
+
+foods = ["Ugali", "Rice", "Mukimo", "Githeri", "Meat"]
+# this variable stores the possible results
+
+print(choice(foods))
+# this will print the result RANDOMLY
